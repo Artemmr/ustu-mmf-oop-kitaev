783fbfb296c750c2c57c57a90c8722ef9bd27523
vokalashnikov@gmail.com <vokalashnikov@gmail.com@130f4f95-eac8-9b0d-c07b-bbc307ebab46>
2014-09-25 19:12:14 +0000

    87

--- trunk/Kalashnikov/C++/example 2/example2/main.cpp
+++ trunk/Kalashnikov/C++/example 2/example2/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <cmath>
 
-int main(int argc, char *argv[])
+using namespace std;
+
+int main()
 {
+    double x,y,z;
+
+    x = 5;
+    y = 4;
+
+    z = sqrt(x*x+y*y);
+
+    cout << "z = " << z ;
+
+    cout << "\n" ;
+
+    double distance = 34000000.0;
+    double lightspeed = 186000.0;
+    double delay;
+    double delay_in_min;
+
+    delay = distance/lightspeed ;
+
+    cout << "delay " << delay << " secon_d.\n";
+
+    delay_in_min = delay / 60.0;
+
+    cout << "it's " << delay_in_min << " min";
 
     return 0;
 }
