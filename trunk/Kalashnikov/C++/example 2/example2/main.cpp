c68ea6318c25c8c150dad9dd106452b60fd1b80d
vokalashnikov@gmail.com <vokalashnikov@gmail.com@130f4f95-eac8-9b0d-c07b-bbc307ebab46>
2014-09-26 10:55:15 +0000

    105, so slow..

--- trunk/Kalashnikov/C++/example 2/example2/main.cpp
+++ trunk/Kalashnikov/C++/example 2/example2/main.cpp
@@ -29,5 +29,48 @@ int main()
 
     cout << "it's " << delay_in_min << " min";
 
+    bool q,p;
+
+    q = true;
+    p = true;
+
+    cout << "\n";
+    cout << p << " XOR " << q << " is " << ((p || q) && !(p && q)) << "\n";
+
+    q = true;
+    p = false;
+
+    cout << p << " XOR " << q << " is " << ((p || q) && !(p && q)) << "\n";
+
+    q = false;
+    p = true;
+
+    cout << p << " XOR " << q << " is " << ((p || q) && !(p && q)) << "\n";
+
+    q = false;
+    p = false;
+
+    cout << p << " XOR " << q << " is " << ((p || q) && !(p && q)) << "\n";
+
+    //type cast
+
+    int i;
+
+    for (i=1; i <= 10; i++){
+        cout << i << " / 2   is " << (float) i/2 << "\n";
+
+    //Payment
+
+    double Principal;
+    double IntRate;
+
+    double PayPerYear;
+    double NumYears;
+    double numer, denom;
+
+    //Pow()
+
+    }
+
     return 0;
 }
