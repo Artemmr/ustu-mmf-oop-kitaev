f7fa1d7608856e0250f48174238190893fd9d3be
vokalashnikov <vokalashnikov@gmail.com>
2014-09-27 06:05:00 +0000

    -> 30.09.2014

--- Kalashnikov/Lab01/main.cpp
+++ Kalashnikov/Lab01/main.cpp
@@ -1,56 +1,19 @@
-#include <QCoreApplication>
-#include <QString>
+#include <iostream>
 
-int b;
+using namespace std;
 
-void VoidFunction(){
-    int a;
-    int b;
-}
+class SignalGenerator {
+public:
+       SignalGenerator (double d); // конструктор
+       virtual double GetSample(); //метод получения последующего сэмпла от генератора
+       virtual void SetFrequency(double iFrequency) = 0; // метод задания частоты генерируемого сигнала
+       virtual void SetAmplitude(double iAmplitude) = 0; // метод задания амплитуды генерируемого сигнала
+       virtual void SetOffset(double iOffset) = 0; // метод задания сдвига фазы относительно начала отсчёта по времени
+       virtual void ResetPosition(); // метод сброса текущего времени
+       virtual void SetDiscretizationFrequency(int iDescrFreq) = 0; // метод задания частоты дискретизации сигнала
+};
 
-int intFunction (int iArg1) {
-    return iArg1/2  ;
-}
+int main (){
 
-double intFunction (double iArg1){
-    return iArg1/2.0 ;
-}
-
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-
-
-    bool bool1;
-
-    bool1 = true;
-    bool1 = false;
-
-    char ch = 'a';
-
-    int ui8 = 12;
-
-    int i16 = -25;
-
-
-
-    int result = intFunction(i16);
-
-    double dresult = intFunction((double)i16);
-
-    int *pi16, **ppi16 ;// пойнтер инт 16
-
-    pi16 = &i16; // взятие адреса
-
-     ppi16 = &pi16;
-    /// в i16 новое значение
-    ///
-    *pi16 = -30;
-
-
-    QString string1;
-
-    string1 = "Это строка 1 " ;
-
-    return a.exec();
+    return 0;
 }
