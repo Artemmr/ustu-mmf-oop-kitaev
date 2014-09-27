1094b2851b4290f6238ed7fdefc03f0b5a688b2c
vokalashnikov@gmail.com <vokalashnikov@gmail.com@130f4f95-eac8-9b0d-c07b-bbc307ebab46>
2014-09-27 06:21:18 +0000

    -> 30.09.2014 update 1

--- trunk/Kalashnikov/Lab01/main.cpp
+++ trunk/Kalashnikov/Lab01/main.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 class SignalGenerator {
 public:
-       SignalGenerator (double d); // конструктор
-       virtual double GetSample(); //метод получения последующего сэмпла от генератора
+       SignalGenerator (void); // конструктор
+       virtual double GetSample(void); //метод получения последующего сэмпла от генератора
        virtual void SetFrequency(double iFrequency) = 0; // метод задания частоты генерируемого сигнала
        virtual void SetAmplitude(double iAmplitude) = 0; // метод задания амплитуды генерируемого сигнала
        virtual void SetOffset(double iOffset) = 0; // метод задания сдвига фазы относительно начала отсчёта по времени
-       virtual void ResetPosition(); // метод сброса текущего времени
+       virtual void ResetPosition(void); // метод сброса текущего времени
        virtual void SetDiscretizationFrequency(int iDescrFreq) = 0; // метод задания частоты дискретизации сигнала
 };
 
