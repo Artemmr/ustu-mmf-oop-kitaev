50ed04374a3fec5df290e63aa2d07e72feee34fc
vokalashnikov@gmail.com <vokalashnikov@gmail.com@130f4f95-eac8-9b0d-c07b-bbc307ebab46>
2014-09-27 10:51:36 +0000

    110: to up!

--- trunk/Kalashnikov/C++/example 2/example2/main.cpp
+++ trunk/Kalashnikov/C++/example 2/example2/main.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
+ #include <iostream>
 #include <cmath>
 
 using namespace std;
 
 int main()
 {
+    /*
     double x,y,z;
 
     x = 5;
@@ -53,10 +54,10 @@ int main()
     cout << p << " XOR " << q << " is " << ((p || q) && !(p && q)) << "\n";
 
     //type cast
-
+/*
     int i;
 
-    for (i=1; i <= 10; i++){
+    for (i=1; i <= 10; i++)
         cout << i << " / 2   is " << (float) i/2 << "\n";
 
     //Payment
@@ -68,8 +69,61 @@ int main()
     double NumYears;
     double numer, denom;
 
-    //Pow()
+    cout << "Enter: Principal,IntRate,PayPerYear,NumYears";
 
+    cin >> Principal;
+    cin >> IntRate;
+    cin >> PayPerYear;
+    cin >> NumYears;
+
+    numer = IntRate * Principal / PayPerYear ;
+
+    e = - (PayPerYear * NumYears);
+    b = (IntRate / PayPerYear) + 1;
+
+
+
+    //Pow()
+*/
+    /*
+     * 1. int , long int, shot int, unsigned int, unsigned shot int, unsigned long int
+     *
+     * 2. float == logn int , just long.
+     *
+     * 3. true,false
+     *
+     * 4. Z : ~-65k <= Z <= ~65k
+     *
+     * 5. \t \a  /// lol , b signall?
+     * */
+
+    /*int i;
+     cout << "\a signall?"; // work
+     cin >> i;
+     cout << "\b signall?"; // Don't work, lol! :)
+     cin >> i;
+     /*
+     *
+     * 6. Y
+     *
+     * 7. lol 1-F
+     *
+     * 8. type var = volue;
+     *
+     * 9.
+     *
+ 0000011   */
+    int i,j,k;
+
+    // first 100
+    for (i=0; i<=100; i++){
+        k = 0.0;
+        for (j=1; j<=i; j++){
+            if (i % j == 0)
+                k++;
+        }
+        if (k == 2)
+            cout << i << " example number\n";
     }
 
     return 0;
