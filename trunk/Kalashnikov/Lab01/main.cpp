959012a1f17637d53d982904ee9863314613cc8c
vokalashnikov@gmail.com <vokalashnikov@gmail.com@130f4f95-eac8-9b0d-c07b-bbc307ebab46>
2014-09-27 06:53:52 +0000

    -> 30.09.2014 update 3 (*.h)

--- trunk/Kalashnikov/Lab01/main.cpp
+++ trunk/Kalashnikov/Lab01/main.cpp
@@ -1,16 +1,8 @@
 #include <iostream>
 
-using namespace std;
+#include <signalgenerator.h>
 
-class SignalGenerator {
-public:
-       virtual double GetSample(void) = 0; //метод получения последующего сэмпла от генератора
-       virtual void SetFrequency(double iFrequency) = 0; // метод задания частоты генерируемого сигнала
-       virtual void SetAmplitude(double iAmplitude) = 0; // метод задания амплитуды генерируемого сигнала
-       virtual void SetOffset(double iOffset) = 0; // метод задания сдвига фазы относительно начала отсчёта по времени
-       virtual void ResetPosition(void) = 0; // метод сброса текущего времени
-       virtual void SetDiscretizationFrequency(int iDescrFreq) = 0; // метод задания частоты дискретизации сигнала
-};
+using namespace std;
 
 int main (){
 
---
+++ trunk/Kalashnikov/Lab01/signalgenerator.h
@@ -0,0 +1,16 @@
+#ifndef SIGNALGENERATOR_H
+#define SIGNALGENERATOR_H
+
+class SignalGenerator
+{
+public:
+    SignalGenerator();
+    virtual double GetSample(void) = 0; //метод получения последующего сэмпла от генератора
+    virtual void SetFrequency(double iFrequency) = 0; // метод задания частоты генерируемого сигнала
+    virtual void SetAmplitude(double iAmplitude) = 0; // метод задания амплитуды генерируемого сигнала
+    virtual void SetOffset(double iOffset) = 0; // метод задания сдвига фазы относительно начала отсчёта по времени
+    virtual void ResetPosition(void) = 0; // метод сброса текущего времени
+    virtual void SetDiscretizationFrequency(int iDescrFreq) = 0; // метод задания частоты дискретизации сигнала
+};
+
+#endif // SIGNALGENERATOR_H
