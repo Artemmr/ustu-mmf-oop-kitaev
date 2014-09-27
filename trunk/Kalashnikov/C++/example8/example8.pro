99dd3991b2734acddeb9c85912dce494e5500472
vokalashnikov@gmail.com <vokalashnikov@gmail.com@130f4f95-eac8-9b0d-c07b-bbc307ebab46>
2014-09-27 06:05:00 +0000

    -> 30.09.2014

---
+++ trunk/Kalashnikov/C++/example8/example8.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-09-27T10:45:48
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = example8
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ trunk/Kalashnikov/C++/example8/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class Vihicle {
+public:
+    int passengers; // pas
+    int fuelcup; // l
+
+    int mpg; // on km
+};
+
+int main()
+{
+    Vihicle minivan; // the object
+
+    int range;
+
+    minivan.passengers = 7;
+    minivan.fuelcup = 16;
+    minivan.mpg = 21;
+
+    range = minivan.fuelcup * minivan.mpg;
+
+    cout << "Minivan can transport " << minivan.passengers << " on " << range << " km. \n";
+
+    return 0;
+}
