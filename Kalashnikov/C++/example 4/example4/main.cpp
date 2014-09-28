93f6c3cf968f087ae85621b856139e2a91802e20
vokalashnikov <vokalashnikov@gmail.com>
2014-09-28 14:24:16 +0000

    161:

--- Kalashnikov/C++/example 4/example4/main.cpp
+++ Kalashnikov/C++/example 4/example4/main.cpp
@@ -1,8 +1,55 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
+using namespace std;
 
-    return a.exec();
+int main(){
+
+    int sample[10];
+
+    int t;
+
+    for (t=0; t<10; ++t)
+        sample[t] = t;
+
+    for (t=0; t<10; ++t)
+        cout << "Index " << t << " Volume: " << sample[t] << "\n";
+
+    int i, avg, min_val, max_val;
+    int num[10];
+
+    for (i = 0; i<10; ++i)
+        num[i] = rand();
+    // AVG
+    avg = 0;
+    for (i = 0; i<10; ++i)
+        avg += num[i];
+    avg /= 10;
+
+    cout << "\navg = " << avg << "\n";
+    // MIN MAX
+    min_val = max_val = num[0];
+
+    for (i = 1; i<10; ++i){
+        if (min_val > num[i])
+            min_val = num[i];
+        if (max_val < num[i])
+            max_val = num[i];
+    }
+
+    cout << "MIN : " << min_val << "\n";
+    cout << "MAX : " << max_val << "\n\n";
+
+    int b[10];
+
+    for (i = 0; i<10; ++i)
+        b[i] = num[i];
+
+    for (i = 0; i<10; ++i)
+        cout << b[i] << "  " << num[i] << "\n";
+
+    cout << "\n\n";
+
+    return 0;
 }
