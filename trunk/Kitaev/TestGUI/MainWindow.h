eb169b58f46a2257e2d22c936aa4b2569909f106
toxin65@gmail.com <toxin65@gmail.com@130f4f95-eac8-9b0d-c07b-bbc307ebab46>
2014-09-30 03:38:42 +0000

    Добавили два менеджера размещения, четыре кнопки, один обработчик нажатия кнопки (слот главного окна).

--- trunk/Kitaev/TestGUI/MainWindow.h
+++ trunk/Kitaev/TestGUI/MainWindow.h
@@ -10,6 +10,10 @@ class MainWindow : public QWidget
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+public slots:
+    void button0Click();
+
 };
 
 #endif // MAINWINDOW_H
