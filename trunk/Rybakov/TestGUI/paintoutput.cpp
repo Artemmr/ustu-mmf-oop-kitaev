bc41387646b4baf199aa66a4cc19b9629b048e85
rib.artem <rib.artem@130f4f95-eac8-9b0d-c07b-bbc307ebab46>
2014-09-30 04:01:38 +0000

    Окно с кнопками и линией

---
+++ trunk/Rybakov/TestGUI/paintoutput.cpp
@@ -0,0 +1,15 @@
+#include "paintoutput.h"
+
+#include <QPainter>
+
+PaintOutput::PaintOutput(QWidget *parent) :
+    QWidget(parent)
+{
+}
+
+void PaintOutput::paintEvent(QPaintEvent *iEvent)
+{
+    QPainter painter(this);
+    painter.setPen(Qt::black);
+    painter.drawLine(0, 0, 100, 100);
+}
