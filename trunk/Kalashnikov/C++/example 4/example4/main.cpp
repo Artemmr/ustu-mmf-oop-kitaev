2922472e2df624fff8d95d5df7a2218a0a5d728d
vokalashnikov@gmail.com <vokalashnikov@gmail.com@130f4f95-eac8-9b0d-c07b-bbc307ebab46>
2014-09-30 17:32:19 +0000

    170:

--- trunk/Kalashnikov/C++/example 4/example4/main.cpp
+++ trunk/Kalashnikov/C++/example 4/example4/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
+#include <cstdio>
 
 using namespace std;
 
@@ -16,14 +17,14 @@ int main(){
     for (t=0; t<10; ++t)
         cout << "Index " << t << " Volume: " << sample[t] << "\n";
 
-    int i, avg, min_val, max_val;
+    int avg, min_val, max_val;
     int num[10];
 
-    for (i = 0; i<10; ++i)
+    for (int i = 0; i<10; ++i)
         num[i] = rand();
     // AVG
     avg = 0;
-    for (i = 0; i<10; ++i)
+    for (int i = 0; i<10; ++i)
         avg += num[i];
     avg /= 10;
 
@@ -31,7 +32,7 @@ int main(){
     // MIN MAX
     min_val = max_val = num[0];
 
-    for (i = 1; i<10; ++i){
+    for (int i = 1; i<10; ++i){
         if (min_val > num[i])
             min_val = num[i];
         if (max_val < num[i])
@@ -43,13 +44,97 @@ int main(){
 
     int b[10];
 
-    for (i = 0; i<10; ++i)
+    for (int i = 0; i<10; ++i)
         b[i] = num[i];
 
-    for (i = 0; i<10; ++i)
+    for (int i = 0; i<10; ++i)
         cout << b[i] << "  " << num[i] << "\n";
 
     cout << "\n\n";
 
+    int nums [3][4];
+
+    for (int t=0; t < 3; ++t){
+        for(int i=0; i < 4; ++i){
+            nums [t][i] = (t*4) + i + 1;
+            cout << nums [t][i] << " " ;
+        }
+        cout << "\n";
+    }
+
+    int nums1[10];
+    int a1, b1, t1;
+    int size1;
+
+    size1 = 10;
+
+    srand(911);
+
+    for (t1 = 0; t1 < size1; t1++)
+        nums1[t1] = rand();
+
+    cout << "start array:\n";
+
+    for (t1 = 0; t1 < size1; t1++)
+        cout << nums1[t1] << "  ";
+
+    cout << '\n';
+
+    for (a1 = 1; a1 < size1; a1++){
+        for (b1 = size1-1; b1 >= a1; b1--) {
+            if (nums1[b1-1] > nums1[b1]){
+                t1 = nums1[b1-1];
+                nums1[b1-1] = nums1[b1];
+                nums1[b1] = t1;
+            }
+        }
+    }
+
+    cout << "after array: \n";
+
+    for (t1 = 0; t1 < size1; t1++)
+        cout << nums1[t1] << "  ";
+
+    cout << "\n\n";
+
+    char str[80];
+
+    cout << "Enter string: ";
+    cin >>  str;
+
+    cout << "U string: ";
+    cout << str;
+
+    cout << "\n\n";
+
+    char rome[90];
+    int q = 1;
+
+    cout << "pls, enter txt: ";
+
+    if (q = 1)
+        gets(rome);
+
+
+    cout << "U string: ";
+    cout << rome;
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
