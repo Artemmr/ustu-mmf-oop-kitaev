c818058b5ad27c265848e3aa46c23e2b01914e0b
toxin65 <toxin65@gmail.com>
2014-09-30 03:38:42 +0000

    Добавили два менеджера размещения, четыре кнопки, один обработчик нажатия кнопки (слот главного окна).

--- Kitaev/TestGUI/MainWindow.cpp
+++ Kitaev/TestGUI/MainWindow.cpp
@@ -1,8 +1,31 @@
 #include "MainWindow.h"
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QPushButton>
 
-MainWindow::MainWindow(QWidget *parent):
-    QWidget(parent)
+MainWindow::MainWindow(QWidget *iParent):
+    QWidget(iParent)
 {
+    QWidget *parent = this;
+    QHBoxLayout *hblayout = new QHBoxLayout(parent);
+    QPushButton *bt0, *bt1;
+    QWidget *leftWidget = new QWidget(parent);
+    hblayout->addWidget(leftWidget);
+    {
+        QWidget *parent = leftWidget;
+        QVBoxLayout *vlay = new QVBoxLayout(parent);
+        vlay->addWidget(new QPushButton("Button 2", parent));
+        vlay->addWidget(new QPushButton("Button 3", parent));
+    }
+    hblayout->addWidget(bt0 = new QPushButton("Button 0", parent));
+    hblayout->addWidget(bt1 = new QPushButton("Button 1", parent));
+
+    connect(bt0, SIGNAL(clicked()), this, SLOT(button0Click()));
+}
+
+void MainWindow::button0Click()
+{
+    this->setWindowTitle("Button0 was clicked");
 }
 
 MainWindow::~MainWindow()
