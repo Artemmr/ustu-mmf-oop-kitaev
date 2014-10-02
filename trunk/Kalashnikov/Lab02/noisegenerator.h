72c94e39aac2dec0f0242411f65b183216c8dd70
vokalashnikov@gmail.com <vokalashnikov@gmail.com@130f4f95-eac8-9b0d-c07b-bbc307ebab46>
2014-10-02 18:01:14 +0000

    Пункт 2

---
+++ trunk/Kalashnikov/Lab02/noisegenerator.h
@@ -0,0 +1,10 @@
+#ifndef NOISEGENERATOR_H
+#define NOISEGENERATOR_H
+
+class NoiseGenerator : public SignalGenerator
+{
+public:
+    NoiseGenerator();
+};
+
+#endif // NOISEGENERATOR_H
---
+++ trunk/Kalashnikov/Lab02/trianglewavegenerator.h
@@ -0,0 +1,10 @@
+#ifndef TRIANGLEWAVEGENERATOR_H
+#define TRIANGLEWAVEGENERATOR_H
+//генератора треугольного сигнала
+class TriangleWaveGenerator : public SignalGenerator
+{
+public:
+    TriangleWaveGenerator();
+};
+
+#endif // TRIANGLEWAVEGENERATOR_H
