b3fb852fcf638cb349299fe7e6bf31c9ebc75a50
vokalashnikov@gmail.com <vokalashnikov@gmail.com@130f4f95-eac8-9b0d-c07b-bbc307ebab46>
2014-10-05 11:19:20 +0000

    -> 07.10.14 , 2 pt update 2

--- trunk/Kalashnikov/Lab02/signalgenerator.h
+++ trunk/Kalashnikov/Lab02/signalgenerator.h
@@ -5,11 +5,11 @@
 class SignalGenerator
 {
 public:
-    virtual double GetSample(void) = 0; //метод получения последующего сэмпла от генератора
+    virtual double GetSample() = 0; //метод получения последующего сэмпла от генератора
     virtual void SetFrequency(double iFrequency) = 0; // метод задания частоты генерируемого сигнала
     virtual void SetAmplitude(double iAmplitude) = 0; // метод задания амплитуды генерируемого сигнала
     virtual void SetOffset(double iOffset) = 0; // метод задания сдвига фазы относительно начала отсчёта по времени
-    virtual void ResetPosition(void) = 0; // метод сброса текущего времени
+    virtual void ResetPosition() = 0; // метод сброса текущего времени
     virtual void SetDiscretizationFrequency(int iDescrFreq) = 0; // метод задания частоты дискретизации сигнала
 };
 
