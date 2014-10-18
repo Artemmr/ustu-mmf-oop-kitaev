600ddd6971a9eacd13c9c170a09669e68f555d0f
rib.artem <rib.artem@130f4f95-eac8-9b0d-c07b-bbc307ebab46>
2014-10-18 15:29:13 +0000

    Добавил класс NoiseGen

--- Rybakov/SignalGenerator/main.cpp
+++ Rybakov/SignalGenerator/main.cpp
@@ -1,8 +1,12 @@
 #include <QCoreApplication>
+#include <signalgen.h>
+#include <noisegen.h>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    //cout<<;
 
     return a.exec();
 }
--- Rybakov/TestGUI/MainWindow.cpp
+++ Rybakov/TestGUI/MainWindow.cpp
@@ -5,22 +5,26 @@
 
 #include "PaintOutput.h"
 
+#include "TestPainter.h"
+
 MainWindow::MainWindow(QWidget *iParent):
     QWidget(iParent)
 {
     QWidget *parent = this;
-    QHBoxLayout *hblayout = new QHBoxLayout(this);
+    QHBoxLayout *hblayout = new QHBoxLayout(parent);
     QPushButton *bt0, *bt1;
-    QWidget *leftWidget = new QWidget(this);
+    QWidget *leftWidget = new QWidget(parent);
     hblayout->addWidget(leftWidget);
     {
         QWidget *parent = leftWidget;
         QVBoxLayout *vlay = new QVBoxLayout(parent);
-        vlay->addWidget(new QPushButton("Кнопка 2", parent));
-        vlay->addWidget(new QPushButton("Кнопка 3", parent));
+        vlay->addWidget(new QPushButton("Button 2", parent));
+        vlay->addWidget(new QPushButton("Button 3", parent));
     }
-    hblayout->addWidget(bt0 = new QPushButton("Кнопка 0", this));
-    hblayout->addWidget(bt1 = new QPushButton("Кнопка 1", this));
+    TestPainter *_painter = new TestPainter(parent);
+    hblayout->addWidget(_painter);
+    hblayout->addWidget(bt0 = new QPushButton("Button 0", parent));
+    hblayout->addWidget(bt1 = new QPushButton("Button 1", parent));
     hblayout->addWidget(new PaintOutput(parent));
 
     connect(bt0, SIGNAL(clicked()), this, SLOT(button0Click()));
@@ -28,7 +32,7 @@ MainWindow::MainWindow(QWidget *iParent):
 
 void MainWindow::button0Click()
 {
-    this->setWindowTitle("Кнопка 0 была нажата");
+    this->setWindowTitle("Button0 was clicked");
 }
 
 MainWindow::~MainWindow()
