a26d9282d54a38e18543d2d901f711569eec8ff6
vokalashnikov <vokalashnikov@gmail.com>
2014-11-14 13:47:37 +0000

    по требованию

--- Kalashnikov/Lab02/mainwindow.cpp
+++ Kalashnikov/Lab02/mainwindow.cpp
@@ -1,14 +1,19 @@
-#include "mainwindow.h"
 #include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QHBoxLayout>
 
+#include "mainwindow.h"
 #include "signalgenerator.h"
 #include "trapezioidwavegenerator.h"
+#include "paintoutput.h"
 
 MainWindow::MainWindow(QWidget *parent) : QWidget(parent) {
-   QWidget *iparent = this;
+
+    QWidget *iparent = this;
+    QHBoxLayout *hblayout = new QHBoxLayout;
    
-   QHBoxLayout *hblayout = new QHBoxLayout;
-   
-   hblayout->addWidget();
+    hblayout->addWidget(ExampleGraphic = new PaintOutput(parent));
+
+    SignalGenerator *ExampleTrapezioidWaveGenerator = new TrapezioidWaveGenerator();
+
+    ExampleTrapezioidWaveGenerator->SetAmplitud
 }
--- Kalashnikov/Lab02/signalgenerator.h
+++ Kalashnikov/Lab02/signalgenerator.h
@@ -5,12 +5,18 @@
 class SignalGenerator
 {
 public:
+
+    enum Result {
+        Success,  /// всё ок
+        BadValue /// не вписывается в ОДЗ
+    }
+    ;
     virtual double GetSample() = 0; //метод получения последующего сэмпла от генератора
-    virtual void SetFrequency(double iFrequency) = 0; // метод задания частоты генерируемого сигнала
+    virtual Result SetFrequency(double iFrequency) = 0; // метод задания частоты генерируемого сигнала
     virtual void SetAmplitude(double iAmplitude) = 0; // метод задания амплитуды генерируемого сигнала
-    virtual void SetOffset(double iOffset) = 0; // метод задания сдвига фазы относительно начала отсчёта по времени
+    virtual Result SetOffset(double iOffset) = 0; // метод задания сдвига фазы относительно начала отсчёта по времени
     virtual void ResetPosition() = 0; // метод сброса текущего времени
-    virtual void SetDiscretizationFrequency(int iDescrFreq) = 0; // метод задания частоты дискретизации сигнала
+    virtual Result SetDiscretizationFrequency(int iDescrFreq) = 0; // метод задания частоты дискретизации сигнала
 };
 
 #endif // SIGNALGENERATOR_H
