03c44b20de4a7d2f0177dab6c64b03ec84431c5b
vokalashnikov <vokalashnikov@gmail.com>
2015-03-30 15:51:28 +0000

    8 : 361

--- Kalashnikov/C++/example 8/example8/main.cpp
+++ Kalashnikov/C++/example 8/example8/main.cpp
@@ -3,27 +3,39 @@
 
 using namespace std;
 
-class Vihicle {
+class Vehicle {
 public:
     int passengers; // pas
-    int fuelcup; // l
-
+    int fuelcap; // l
     int mpg; // on km
+
+    int range();
 };
 
 int main()
 {
-    Vihicle minivan; // the object
+    Vehicle minivan; // the object
+    Vehicle sportcar;
 
-    int range;
+//    int range;
 
     minivan.passengers = 7;
-    minivan.fuelcup = 16;
+    minivan.fuelcap = 16;
     minivan.mpg = 21;
 
-    range = minivan.fuelcup * minivan.mpg;
+    sportcar.passengers = 2;
+    sportcar.fuelcap = 14;
+    sportcar.mpg = 12;
 
-    cout << "Minivan can transport " << minivan.passengers << " on " << range << " km. \n";
+//    range = minivan.fuelcap * minivan.mpg;
+    cout << "Minivan can transport " << minivan.passengers << " on " << minivan.range() << " km. \n";
+
+//    range = sportcar.fuelcap * sportcar.mpg;
+    cout << "Sportcar can transport " << sportcar.passengers << " on " << sportcar.range() << " km. \n";
 
     return 0;
+}
+
+int Vehicle::range(){
+    return fuelcap*mpg;
 }
