2d5984e389f6a4ca0a0204fbe06c51e49bc5c990
45grammov <45grammov@gmail.com>
2015-04-07 04:58:26 +0000

    Создал абстрактный класс

---
+++ Malcev/Lab02/signalgenerator.h
@@ -0,0 +1,16 @@
+﻿#ifndef SIGNALGENERATOR_H
+#define SIGNALGENERATOR_H
+
+
+class SignalGenerator
+{
+public:
+    virtual double GetSample() = 0;
+    virtual void ResetPostion() = 0;
+    virtual void SetAmplitude(double iAmplitude) = 0;
+    virtual void SetFrequency(double iFrequency) = 0;
+    virtual void SetOffset(double iOffset) = 0;
+    virtual void SetDescretizationFrequency(int iDescrFreq) = 0;
+};
+
+#endif // SIGNALGENERATOR_H
